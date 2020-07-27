776a09785e04646610e704610a7d04de0acbb6a3
tipplerow <jsshaff@berkeley.edu>
2020-07-27 14:51:09 -0700

    Retrieve a map of the file records on demand

--- src/pubmed/bulk/MultiContentFile.java
+++ src/pubmed/bulk/MultiContentFile.java
@@ -4,8 +4,14 @@ package pubmed.bulk;
 import java.util.ArrayList;
 import java.util.List;
 
-import jam.flat.FlatRecord;
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ImmutableListMultimap.Builder;
+import com.google.common.collect.ListMultimap;
 
+import jam.flat.RecordStore;
+
+import pubmed.article.PMID;
+import pubmed.flat.PubmedFlatRecord;
 import pubmed.xml.PubmedArticleElement;
 import pubmed.xml.PubmedXmlDocument;
 
@@ -13,7 +19,9 @@ import pubmed.xml.PubmedXmlDocument;
  * Provides a base class to for flat files containing multiple joining
  * records (many-to-many mappings) for each article.
  */
-public abstract class MultiContentFile<V extends FlatRecord> extends DocumentContentFile<V> {
+public abstract class MultiContentFile<V extends PubmedFlatRecord> extends DocumentContentFile<V> {
+    private ListMultimap<PMID, V> recordMap = null;
+
     /**
      * Creates a new flat file for records derived from a given bulk
      * XML file.
@@ -34,6 +42,34 @@ public abstract class MultiContentFile<V extends FlatRecord> extends DocumentCon
      * empty if the article does not contain any relevant data items).
      */
     public abstract List<V> extractRecords(PubmedArticleElement element);
+
+    /**
+     * Returns an immutable multimap of the records in this file
+     * indexed by {@code PMID}.  The file will be generated on demand
+     * if necessary.
+     *
+     * @return an immutable map of the records in this file indexed
+     * by {@code PMID}.
+     */
+    public synchronized ListMultimap<PMID, V> getRecordMap() {
+        if (recordMap == null)
+            mapRecords();
+
+        return recordMap;
+    }
+
+    private void mapRecords() {
+        if (!exists())
+            processFile(false);
+
+        RecordStore<V> recordStore = load();
+        ImmutableListMultimap.Builder<PMID, V> builder = ImmutableListMultimap.builder();
+
+        for (V record : recordStore)
+            builder.put(record.getPMID(), record);
+
+        recordMap = builder.build();
+    }
 
     @Override public List<V> extractRecords(PubmedXmlDocument document) {
         List<V> documentRecords = new ArrayList<V>();
--- src/pubmed/bulk/UniqueContentFile.java
+++ src/pubmed/bulk/UniqueContentFile.java
@@ -1,11 +1,16 @@
 
 package pubmed.bulk;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
-import jam.flat.FlatRecord;
+import jam.flat.RecordStore;
 import jam.util.StreamUtil;
 
+import pubmed.article.PMID;
+import pubmed.flat.PubmedFlatRecord;
 import pubmed.xml.PubmedArticleElement;
 import pubmed.xml.PubmedXmlDocument;
 
@@ -13,7 +18,9 @@ import pubmed.xml.PubmedXmlDocument;
  * Provides a base class to for flat files containing unique article
  * attribute data (one row per article).
  */
-public abstract class UniqueContentFile<V extends FlatRecord> extends DocumentContentFile<V> {
+public abstract class UniqueContentFile<V extends PubmedFlatRecord> extends DocumentContentFile<V> {
+    private Map<PMID, V> recordMap = null;
+
     /**
      * Creates a new flat file for records derived from a given bulk
      * XML file.
@@ -36,6 +43,34 @@ public abstract class UniqueContentFile<V extends FlatRecord> extends DocumentCo
      * the relevant data items).
      */
     public abstract V extractRecord(PubmedArticleElement element);
+
+    /**
+     * Returns an unmodifiable map of the records in this file indexed
+     * by {@code PMID}.  The file contents will be generated on demand
+     * if necessary.
+     *
+     * @return an unmodifiable map of the records in this file indexed
+     * by {@code PMID}.
+     */
+    public synchronized Map<PMID, V> getRecordMap() {
+        if (recordMap == null)
+            mapRecords();
+
+        return recordMap;
+    }
+
+    private void mapRecords() {
+        if (!exists())
+            processFile(false);
+
+        RecordStore<V> recordStore = load();
+        recordMap = new LinkedHashMap<PMID, V>(recordStore.count());
+
+        for (V record : recordStore)
+            recordMap.put(record.getPMID(), record);
+
+        recordMap = Collections.unmodifiableMap(recordMap);
+    }
 
     @Override public List<V> extractRecords(PubmedXmlDocument document) {
         return StreamUtil.applyParallel(document.getPubmedArticleElements(), element -> extractRecord(element));
